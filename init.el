5c459c61147952d0b34b5600184c33fba2a92300
Carl Bolduc <contact@carlbolduc.com>
2020-06-04 16:10:56 -0400

    Set web mode indentation

--- init.el
+++ init.el
@@ -132,7 +132,10 @@ _!_: errors
   :config
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.php?\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.twig?\\'" . web-mode)))
+  (add-to-list 'auto-mode-alist '("\\.twig?\\'" . web-mode))
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
 (add-hook
  'web-mode-hook
  (lambda ()
